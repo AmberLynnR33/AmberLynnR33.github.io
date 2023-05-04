69ff1c4d148ab862cae7b398e93d25c172adad0e
AmberLynnR33 <103320792+AmberLynnR33@users.noreply.github.com>
2023-05-04 13:07:06 -0400

    Update all page changes
    - Able to move to each of the three main  pages (the home page, the projects page, and the blogs page)

--- changing_pages.js
+++ changing_pages.js
@@ -2,3 +2,9 @@ function projectsPage() {
     window.location.href = 'projects_page.html'
 
 }
+function blogsMainPage() {
+    window.location.href = "blog_main_page.html"
+}
+function homePage() {
+    window.location.href = "main_page.html"
+}
