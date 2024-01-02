a55514a4d18c19eec07284fceb986c53ab942b5a
AmberLynnR33 <103320792+AmberLynnR33@users.noreply.github.com>
2024-01-02 17:38:54 -0500

    Updates to colour and renaming
    - Renamed header_styles to general_styles to better reflect what the file contained
    - Played around with the colours on index.html
    - Deleted some unused javascript
    - Renamed the css for fonts on the website

--- javascript/changing_pages.js
+++ javascript/changing_pages.js
@@ -1,15 +1,3 @@
-function projectsPage() {
-    var newLocation = "html/projects_page.html";
-    window.location.href = newLocation;
-}
-function blogsMainPage() {
-    var newLocation = "html/blog_main_page.html";
-    window.location.href = newLocation;
-}
-function homePage() {
-    var newLocation = "../index.html";
-    window.location.href = newLocation;
-}
 function projectWebsite() {
     window.open("https://github.com/AmberLynnR33/ambear-website");
 }
@@ -22,9 +10,4 @@ function githubLink() {
 function blogTemplate() {
     window.location.href = "./all_blogs/blog_template.html"
 }
-
-
-
-var blog_to_ind = document.getElementById('ind_from_blog');
-blog_to_ind.onclick = homePage
 
