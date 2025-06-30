a506e85efac923b52be069a570b950106a5b6d4d
AmberLynnR33 <103320792+AmberLynnR33@users.noreply.github.com>
2025-06-30 14:45:56 -0400

    Creating main page rough layout

--- javascript/mobile_web.js
+++ javascript/mobile_web.js
@@ -1,5 +1,6 @@
 function handleResize() {
     const container = document.getElementsByClassName('stackable_container');
+    var dividers = document.getElementsByClassName('subtitle_text')
     var navi = document.querySelector('.navbar_style');
     var navi_2 = document.querySelector('.unstacked_navbar')
     const breakpoint = 768;
@@ -11,12 +12,20 @@ function handleResize() {
         if (navi) {
             navi.className = 'unstacked_navbar';
         }
+        for (var i=0; i<dividers.length; i++){
+            dividers[i].classList.remove('right_divider');
+            dividers[i].classList.add('underline_text');
+        }
     } else {
         for (var i=0; i<container.length; i++){
             container[i].classList.remove('stacked_layout');
         }
         if(navi_2){
             navi_2.className = 'navbar_style';
+        }
+        for (var i=0; i<dividers.length; i++){
+            dividers[i].classList.add('right_divider');
+            dividers[i].classList.remove('underline_text');
         }
     }
 }
